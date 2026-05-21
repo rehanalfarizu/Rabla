1e08b5320cfb16614fd0b58210f788f53d2cfb88
rehanalfarizu <rehanalfarizi@students.amikom.ac.id>
2026-05-21 19:15:35 +0700

    Build Vue to public/build, serve via Laravel route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    // Serve Vue SPA from public/build folder
+    $indexPath = public_path('build/index.html');
+    if (file_exists($indexPath)) {
+        return response(file_get_contents($indexPath), 200)
+            ->header('Content-Type', 'text/html');
+    }
+    return response('Vue app not built', 500);
 });
 
 Route::get('/debug-session', function() {
